612bdae7754cce64aa1aaedae5b35854dd6a2541
jayymccants@gmail.com <jayymccants@gmail.com>
2024-03-20 19:54:02 -0400

    Passed the DOM manipulation tests

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+const mainElement = document.getElementById("main");
+newHeader.textContent = "Jay is the champion";
+mainElement.remove();
